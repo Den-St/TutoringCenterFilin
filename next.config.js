d30a1d43f37827436083b00998a5f697f449704a
denisstuenko <stuenkden@gmail.com>
2023-10-27 10:49:35 +0300

    trying to update deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    basePath: '/TutoringCenterFilin',
 }
 
 module.exports = nextConfig
